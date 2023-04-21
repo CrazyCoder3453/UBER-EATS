af1f9436eabe90d8312ecafbca5af30799f2d58e
= <=>
2023-04-20 21:17:43 -0500

    Tests

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -8,7 +8,7 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ src, title, description }) => {
   return (
-    <div className="w-80 h-96 bg-gray-100 rounded-md p-4 ml-9">
+    <div className="w-96 h-96 bg-gray-100 rounded-md p-4 ml-9">
       <img className="w-full h-56 object-cover rounded-md" src={src} alt={title} />
       <h2 className="mt-2 text-lg font-medium">{title}</h2>
       <p className="mt-2 text-sm text-gray-500">{description}</p>
